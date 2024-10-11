996487ab92356d7e00b7b1276643ee437b9bf116
FreitasProcopio <pp918545@gmail.com>
2024-10-11 16:28:38 -0300

    :sparkles: feat: Add new recurses for page

---
+++ src/js/index.js
@@ -0,0 +1,80 @@
+
+
+// function pegar_dados() {
+    
+//     var tasks = document.getElementById('text_task').value;
+//     localStorage.setItem('Frase', tasks);
+
+//     const frases = localStorage.getItem('Frase');
+
+//     arrayItens(frases)
+// }
+
+
+// function arrayItens(frases) {
+
+//     let mensagens = JSON.parse(localStorage.getItem('BD')) || [];
+
+//     const BD = {
+//         id: frases.length+1,
+//         msg: frases
+//     }; 
+
+//     mensagens.push(BD);
+
+//     // localStorage.setItem('BD',JSON.stringify(mensagens));
+//     const nomes = BD.msg;
+//     localStorage.setItem('nomes', JSON.stringify(nomes));
+
+//     arrayExibir(nomes);
+
+//     return BD;
+// }
+
+
+// function arrayExibir(nomes) {
+
+    
+//     document.getElementById('text_recebe').textContent = nomes.join(', ');;
+// }
+
+// pegar_dados();
+
+
+function pegar_dados() {
+    var tasks = document.getElementById('text_task').value;
+    
+    // Adiciona a nova tarefa no localStorage
+    const frases = localStorage.getItem('BD') ? JSON.parse(localStorage.getItem('BD')) : [];
+
+    // ? -> Operador ternário
+    // Se for encontrado alguma para em 'BD', execute o comando JSON.parse... . Se for falso, corre para o [] e cria uma array de objetos;
+
+
+    arrayItens(tasks, frases);
+}
+
+function arrayItens(frases, mensagens) {
+
+    const BD = {
+        id: mensagens.length + 1,
+        msg: frases
+    
+    };
+    mensagens.push(BD);
+    
+    localStorage.setItem('BD', JSON.stringify(mensagens));
+
+    // Coleta todas as mensagens para exibição
+    const nomes = mensagens.map(item => item.msg);
+    
+    // Para cada item no array mensagens, ele pega apenas o valor da mensagem 'item.msg'
+    localStorage.setItem('nomes', JSON.stringify(nomes));
+
+    arrayExibir(nomes);
+}
+
+function arrayExibir(nomes) {
+    document.getElementById('text_recebe').textContent = nomes.join(', ');
+}
+
